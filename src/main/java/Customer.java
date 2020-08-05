18329d50698a39dabb4ba6930e67d2892a84d834
MarinaB2 <44431361+MarinaB2@users.noreply.github.com>
2020-08-05 23:30:21 +0200

    project

---
+++ src/main/java/Customer.java
@@ -0,0 +1,40 @@
+
+    public class Customer {
+
+    private String customerId;
+    private String firstName;
+    private String lastName;
+
+        public Customer(String customerId, String firstName, String lastName) {
+            this.customerId = customerId;
+            this.firstName = firstName;
+            this.lastName = lastName;
+
+        }
+        public String getCustomerId() {
+            return customerId;
+        }
+
+        public void setCustomerId(String customerId) {
+            this.customerId = customerId;
+        }
+
+        public String getFirstName() {
+            return firstName;
+        }
+
+        public void setFirstName(String firstName) {
+            this.firstName = firstName;
+        }
+
+        public String getLastName() {
+            return lastName;
+        }
+
+        public void setLastName(String lastName) {
+            this.lastName = lastName;
+        }
+
+
+
+    }
---
+++ src/main/java/CustomerDatabase.java
@@ -0,0 +1,211 @@
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
+
+public class CustomerDatabase {
+
+    // Setup
+    private static String URL = "jdbc:sqlite::resource:Chinook_Sqlite.sqlite";
+    private static Connection conn = null;
+    public static ArrayList<Customer> customers = new ArrayList<Customer>();
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a customer Id: ");
+        int userInput;
+        Customer customer;
+
+        if (scanner.hasNextInt()){//Checking if the input is an integer
+            userInput= scanner.nextInt();
+            customer = getCustomerName(userInput);
+
+        } else{
+               System.out.println("Getting a random customer ..");
+               customer = getRandomCustomerName();
+               userInput = Integer.parseInt(customer.getCustomerId());
+
+           }
+        System.out.println("The full name of the customer is: " + customer.getFirstName() + " " + customer.getLastName());
+
+
+        ArrayList<String> getgeneres = getGenreName(userInput);
+        System.out.println("This all the generes that the customers like, in order " + getgeneres);
+
+
+        ArrayList<String> generes = getMostPopularGenre(userInput);
+
+            System.out.println("Customers most popular genre is : " + generes.get(generes.size() -1));
+
+            for(String genre : generes){
+                if(genre.length() == generes.size()-1){
+                    System.out.println("Customers have another most popular genre is : " + generes.get(generes.size() -2));
+                }
+            }
+
+    }
+
+    public static Customer getCustomerName(int customerId) {
+        try {
+            conn = DriverManager.getConnection(URL);
+
+            // Prepare Statement
+            PreparedStatement preparedStatement =
+                    conn.prepareStatement("SELECT CustomerId,FirstName,LastName FROM Customer WHERE CustomerId =?");
+            preparedStatement.setInt(1, customerId);
+
+            // Execute Statement
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+
+                     return new Customer(
+                                resultSet.getString("customerId"),
+                                resultSet.getString("firstName"),
+                                resultSet.getString("lastName")
+                     );
+
+
+
+        } catch (Exception ex) {
+            System.out.println("Something went wrong(CustomerName)...");
+            System.out.println(ex.toString());
+        } finally {
+            try {
+                // Close Connection
+                conn.close();
+            } catch (Exception ex) {
+                System.out.println("Something went wrong(CustomerName) while closing connection.");
+                System.out.println(ex.toString());
+            }
+        }
+        return null;
+    }
+
+    public static Customer getRandomCustomerName() {
+        try {
+            conn = DriverManager.getConnection(URL);
+
+            // Prepare Statement
+            PreparedStatement preparedStatement =
+                    conn.prepareStatement("SELECT * FROM Customer");
+
+            // Execute Statement
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            // Process Results
+            while (resultSet.next()) {
+                customers.add(
+                        new Customer(
+                                resultSet.getString("customerId"),
+                                resultSet.getString("firstName"),
+                                resultSet.getString("lastName")
+                        ));
+            }
+            int random = new Random().nextInt(customers.size());
+            return customers.get(random);
+
+        } catch (Exception ex) {
+            System.out.println("Something went wrong(CustomerName)...");
+            System.out.println(ex.toString());
+        } finally {
+            try {
+                // Close Connection
+                conn.close();
+            } catch (Exception ex) {
+                System.out.println("Something went wrong(CustomerName) while closing connection.");
+                System.out.println(ex.toString());
+            }
+        }
+        return null;
+    }
+
+    public static ArrayList<String> getGenreName(int customerId) {
+        try {
+            // Open Connection
+            conn = DriverManager.getConnection(URL);
+
+            // Prepare Statement
+            //COUNT(Invoice.InvoiceId),
+            PreparedStatement preparedStatement =
+                    conn.prepareStatement("SELECT Genre.Name FROM Genre JOIN Track ON Genre.GenreId = Track.GenreId "
+                            + "JOIN InvoiceLine ON InvoiceLine.TrackId = Track.TrackId "
+                            + "JOIN Invoice ON InvoiceLine.InvoiceId = Invoice.InvoiceId "
+                            + "WHERE CustomerId =? " + "GROUP BY Genre.Name " + "ORDER BY COUNT(Invoice.InvoiceId) DESC");
+            //+ "GROUP BY Genre.Name " + "ORDER BY COUNT(Invoice.InvoiceId) DESC"
+            preparedStatement.setInt(1, customerId);
+
+            // Execute Statement
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            ArrayList<String> geners = new ArrayList<>();
+            // Process Results
+            while (resultSet.next()) {
+                geners.add(resultSet.getString("Name"));
+
+            }
+            return geners;
+        } catch (Exception ex) {
+            System.out.println("Something went wrong(getGenreName)...");
+            System.out.println(ex.toString());
+        } finally {
+            try {
+                // Close Connection
+                conn.close();
+            } catch (Exception ex) {
+                System.out.println("Something went wrong(getGenreName) while closing connection.");
+                System.out.println(ex.toString());
+            }
+        }
+        return null;
+    }
+
+    public static ArrayList<String> getMostPopularGenre(int customerId) {
+        try {
+            // Open Connection
+            conn = DriverManager.getConnection(URL);
+            // Prepare Statement
+            PreparedStatement preparedStatement =
+                    conn.prepareStatement(" SELECT Genre.Name, COUNT(Invoice.InvoiceId), Customer.FirstName, Genre.GenreId FROM Invoice "
+                            + "JOIN Customer ON Invoice.CustomerId = Customer.CustomerId "
+                            + "JOIN InvoiceLine ON InvoiceLine.InvoiceId = Invoice.InvoiceId "
+                            + "JOIN Track ON Track.TrackId = InvoiceLine.TrackId "
+                            + "JOIN Genre ON Track.GenreId = Genre.GenreId "
+                            + "WHERE Customer.CustomerId =? " + "GROUP BY Genre.Name " + "ORDER BY COUNt(Invoice.InvoiceId)" );
+            preparedStatement.setInt(1, customerId);
+
+            // Execute Statement
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            ArrayList<String> geners = new ArrayList<>();
+            // Process Results
+            while (resultSet.next()) {
+                geners.add(resultSet.getString("Name"));
+
+            }
+            return geners;
+
+        } catch (Exception ex) {
+            System.out.println("Something went wrong(MostPopularGenre)...");
+            System.out.println(ex.toString());
+
+        } finally {
+            try {
+                // Close Connection
+                conn.close();
+            } catch (Exception ex) {
+                System.out.println("Something went wrong(MostPopularGenre) while closing connection.");
+                System.out.println(ex.toString());
+            }
+        }
+        return null;
+    }
+
+}
+
+
